940bbadcf6dae0da1193c8d02d89969d493f2c41
dickxumamba <xuruoyu27@gmail.com>
2025-09-16 15:14:52 +0800

    Updated

--- lab11.cpp
+++ lab11.cpp
@@ -4,8 +4,9 @@ using namespace std;
 const int recent_20ratings = 20; 
 
 struct Shop{ //Create our struct which is called "Shop" and stores the basic info for it. 
+    
     string name;
-    string address;
+    int ID;
     double *ratings; // A dynamic array the stores the ratings of the shop
 
 };
@@ -17,13 +18,20 @@ int main(){
      //We only keep the most recent 20 ratings stored in the array of the shop.
     Shop Muji; // Create a shop called "Muji"
     Muji.ratings = new double[recent_20ratings];
+    
     return 0;
 }
 
 void input_shop(Shop *Muji){
-    string ipt;
+    // Prompt the user to populate his/her shop, finish all the basic info.
     cout << "Input the name for the Shop: "<<endl;
     getline(cin, Muji->name);
+    cout << "Provide your shop ID: "<<endl;
+    cin >> Muji->ID;
 }
 
-void display_shop(Shop *){}
+void display_shop(Shop *Muji){
+    cout << "The name of the shop is: " << Muji->name << endl;
+    cout << "Shop ID:# " << Muji->ID << endl;
+
+}
