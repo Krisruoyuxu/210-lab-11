d2559b8bd79ad9d0c358b54507f3de142c1fb8a1
dickxumamba <xuruoyu27@gmail.com>
2025-09-16 14:53:01 +0800

    Updated, working on the whole structure of the program

--- lab11.cpp
+++ lab11.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 
-
+const int recent_20ratings = 20; 
 
 struct Shop{ //Create our struct which is called "Shop" and stores the basic info for it. 
-   
+    string name;
+    string address;
+    double *ratings; // A dynamic array the stores the ratings of the shop
 
 };
 
@@ -12,8 +14,7 @@ void input_shop(Shop *);
 void display_shop(Shop *);
 
 int main(){
-    
-    //
-    
+     //We only keep the most recent 20 ratings stored in the array of the shop.
+    Shop *KFC = new Shop[recent_20ratings];
     return 0;
 }
