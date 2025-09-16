8a2254c388e5bc02cb37330c285af28440a56ae3
dickxumamba <xuruoyu27@gmail.com>
2025-09-16 14:40:07 +0800

    start the lab11

---
+++ lab11.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+
+struct Shop{ //Create our struct which is called "Shop" and stores the basic info for it. 
+   
+
+};
+
+void input_shop(Shop *);
+void display_shop(Shop *);
+
+int main(){
+    
+    //
+    
+    return 0;
+}
