475d5414496bf57292b1031d5350b50292c94faa
dickxumamba <xuruoyu27@gmail.com>
2025-09-21 16:43:46 +0800

    updated

--- lab11.cpp
+++ lab11.cpp
@@ -33,9 +33,16 @@ int main(){
     for (int i = 0; i < recent_20ratings; i++){  // traverse the file and add the first 20 elements into the array 
         fin >> Muji.ratings[i];
     }
-
+    
     input_shop(&Muji);
     display_shop(&Muji);
+
+    for (int i = 10; i < recent_20ratings+10; i++)
+        fin >> Muji.ratings[i]; // read the last 20 elements in the file(that represents the new 20 ratings
+    // and display it agian
+    display_shop(&Muji);
+    
+    fin.close(); // close the file
 
     return 0;
 }
